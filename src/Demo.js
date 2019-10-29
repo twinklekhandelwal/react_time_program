32c54ae7a976833869083096b2384739c17a2f00
amarkrsinha1997 <amar17@navgurukul.org>
2019-10-29 12:25:05 +0530

    use lifecycle

--- src/Demo.js
+++ src/Demo.js
@@ -1,34 +1,33 @@
 import React from 'react';
 
-// class Demo extends React.Component{
-//     render(){
-//         return(
-//             <h1>twinkle is a fronted developer</h1>)
+class Demo extends React.Component{
+  constructor(props){
+  super(props);
+  this.state={"roll":101}
+
+  }
+  update_roll = () => (
+    this.setState((state)=> (
+      {roll:state.roll+1}
+  ))
+  )
+  componentDidUpdate(prevProps,prevstate){
+    console.log("change");
+  }
+  componentDidMount(){
+    console.log("use for api");
+  }
+    render(){
+        return(
+          <div>
+            <h1>roll number of twinkle{this.state.roll}</h1>
+            <button onClick={this.update_roll}> roll number</button>
+            </div>
+            
+            )
         
-//     }
-// }
-// export default Demo;
-function Wel(props) {
-  return <div><h1>Hello, {props.name}</h1>
-  <h2>It is {new Date().toLocaleTimeString()}.pm</h2></div>
-}
-function tic() {
-    
-    return(
-    
-      <div>
-        <Wel name="Twinkle" />
-        <Wel name="Amar" />
-        <Wel name="Diwakar" />
-      
-        {/* <h2>It is {new Date().toLocaleTimeString()}.pm</h2> */}
-      </div>)
-    
-   
-  
+    }
 }
 
 
-  
-
-export default tic;
+export default Demo;
